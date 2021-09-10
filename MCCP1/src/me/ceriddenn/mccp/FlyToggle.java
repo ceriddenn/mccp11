ce1dfdd6f9ecaaa7695eaf5f429af41701dd8150
ceriddenn <90457772+ceriddenn@users.noreply.github.com>
2021-09-10 15:20:01 -0400

    Fly class pushed to branch origin

---
+++ MCCP1/src/me/ceriddenn/mccp/FlyToggle.java
@@ -0,0 +1,38 @@
+package me.ceriddenn.mccp;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class FlyToggle implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Player player = (Player) sender;
+		if (sender instanceof Player) {
+				Main.flytoggle.add(player.getUniqueId());
+				player.setAllowFlight(true);
+				
+			} else {
+				player.sendMessage("You did not provide a player!");
+			}
+			if (Main.flytoggle.contains(player.getUniqueId())) {
+				Main.flytoggle.remove(player.getUniqueId());
+				player.setAllowFlight(true);
+				
+				
+						
+						
+						
+				
+			}
+			
+			
+			
+			
+			
+		return false;
+	}
+
+}
